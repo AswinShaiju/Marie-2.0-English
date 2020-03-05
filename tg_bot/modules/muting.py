dc5dce0ed276838a8f4ec298082988909f7fc511
AswinShaiju <60039486+AswinShaiju@users.noreply.github.com>
2020-03-05 22:22:51 +0530

    Created

--- tg_bot/modules/muting.py
+++ tg_bot/modules/muting.py
@@ -40,7 +40,7 @@ def mute(bot: Bot, update: Update, args: List[str]) -> str:
 
         elif member.can_send_messages is None or member.can_send_messages:
             bot.restrict_chat_member(chat.id, user_id, can_send_messages=False)
-            message.reply_text("👍🏻 muted! 🤐")
+            message.reply_text("👍muted!🤐")
             return "<b>{}:</b>" \
                    "\n#MUTE" \
                    "\n<b>Admin:</b> {}" \
