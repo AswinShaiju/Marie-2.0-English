e3bd3b72ff62883955fc877eb841fb346dd23f09
AswinShaiju <60039486+AswinShaiju@users.noreply.github.com>
2020-03-20 07:58:45 +0530

    Created

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -21,9 +21,8 @@ PM_START_TEXT = """
 Hello there! {}, My name is {} - I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
 
 Want to add me to your chat? [Click here!](http://telegram.me/yourbot?startgroup=botstart)
-
 """
-HELP_STRINGS = """
+HELP_STRINGS = ""
 
 Hello! my name *{}*.
 
